cf350b1769d8258daff655008b89cc5263519116
Pujit Mehrotra <pmehrotra2@babson.edu>
2021-11-08 15:45:13 -0500

    Finish assignment 3

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # WebApp-MBTA
- This is the base repo for MBTA project. Please read [instructions](instructions.md). 
+
+This is the base repo for MBTA project. Please read [instructions](instructions.md).
+
+notes on mbta api usage
+
+my first instinct was that making an api call everytime we want to find the nearest station is way overkill because stations dont change all that often.
+then i ran the stops endpoint and got a 10mb response in return, which pissed me off cuz thats poor design
+it's way better to do some data pre-processing to make things efficient.
+
+hence the static json files
+more memory on server is cheaper than bandwidth (in most cases)
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,55 +1,103 @@
-# Useful URLs (you need to add the appropriate parameters for your requests)
-MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
-MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
-
-# Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+import json
+import requests
+from dotenv import load_dotenv
+from os import environ
+from scipy import spatial
 
 
-# A little bit of scaffolding if you want to use it
+def create_url_builder(base_url):
+    """Quick, shitty tool to build api endpoints.
 
-def get_json(url):
+    Pass in base api url to get a new function. Pass it an endpoint and get the fully formed url path.
+
+    Args:
+        base_url (string): domain of api. Prefer to omit trailing slash.
+
+    Returns:
+        func(api_route (string)) -> string : Returns full url of api endpoint
     """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
+    return lambda route: base_url + route
+
+
+load_dotenv()
+MAPQUEST_API_KEY = environ.get("MAPQUEST_API_KEY")
+MAPQUEST_BASE_URL = environ.get("MAPQUEST_BASE_URL")
+
+
+def prepare_mbta_stop_data(stops):
+    """Read MBTA station data (megabytes) and parse into read-only data collections for querying and access
+
+    Args:
+        stops (list[station_data]): Pre-processed MBTA station data. List of JSON objects.
+
+    Returns:
+        dictionary: {
+            stops(list[station_data]): Pre-processed MBTA station data. List of JSON objects. Reference to parameter,
+            stop_coordinates_tree (scipy.spatial.KDTree): (float, float) lat,long coordinate tuples linked to each entry in `stops`
+        }
     """
-    pass
+    get_coordinates = lambda stop: (stop["lat"], stop["long"])
+    stop_coordinates = [get_coordinates(stop) for stop in stops]
+    return {
+        "stops": stops,
+        "stop_coordinates_tree": spatial.KDTree(stop_coordinates),
+    }
 
 
 def get_lat_long(place_name):
+    """Given a string representing a place, return (latitude, longitude) tuple
+
+    Args:
+        place_name (string): Arbitrary place (e.g. Washington D.C.)
+
+    Returns:
+        (float, float): Latitude, Longitude coordinate pair
     """
-    Given a place name or address, return a (latitude, longitude) tuple
-    with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding  API URL formatting requirements.
-    """
-    pass
+    url_params = {"key": MAPQUEST_API_KEY, "location": place_name}
+    endpoint = create_url_builder(MAPQUEST_BASE_URL)("/address")
+    response = requests.get(endpoint, params=url_params).json()
+    coordinates = response["results"][0]["locations"][0]["latLng"]
+    return (coordinates["lat"], coordinates["lng"])
 
 
-def get_nearest_station(latitude, longitude):
+def get_nearest_station(coordinates, stops_collections):
+    """Given latitude,longitude coordinates, find nearest station in `stop_collections`
+
+    Args:
+        coordinates (float, float): Latitude, Longitude
+        stops_collections (IStop_Collections): see `prepare_mbta_stop_data` for interface shape
+
+    Returns:
+        dictionary/object: info representing an MBTA station
     """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
-    tuple for the nearest MBTA station to the given coordinates.
-    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
-    formatting requirements for the 'GET /stops' API.
-    """
-    pass
+    coordinate_tree = stops_collections["stop_coordinates_tree"]
+    # https://stackoverflow.com/a/39109296/6656631 -- using k-dim trees to find closest coordinate pair in set
+    results = coordinate_tree.query([coordinates])
+    # K-Dimensional Tree Query Parsing -- https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.query.html#scipy.spatial.KDTree.query
+    stop_index = list(results[1])[0]
+    return stops_collections["stops"][stop_index]
 
 
-def find_stop_near(place_name):
+def find_stop_near(place_name, stop_collections):
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
     """
-    pass
+    return get_nearest_station(
+        get_lat_long(place_name), stops_collections=stop_collections
+    )
 
 
 def main():
     """
     You can test all the functions here
     """
-    pass
+    from_filename = "filtered_mbta_stop_data.json"
+    with open(from_filename) as stop_data_file:
+        stops = json.load(stop_data_file)
+        collections = prepare_mbta_stop_data(stops=stops)
+        print(find_stop_near("Babson College", stop_collections=collections))
+        print(find_stop_near("Boston", stop_collections=collections))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
